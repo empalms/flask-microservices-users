9bc22adf9864279420ab2d052a7c6369b0716649
empalms <ev@npalmer.io>
2017-10-09 21:33:22 -0500

    containerized flask app

--- project/__init__.py
+++ project/__init__.py
@@ -1,11 +1,14 @@
+import os
 from flask import Flask, jsonify
 
 # instantiate the application
 app = Flask(__name__)
 
+
+# pull in environment variables
+app_settings = os.getenv('APP_SETTINGS')
 # set configuration from config.py
 app.config.from_object('project.config.DevelopmentConfig')
-
 
 
 @app.route('/ping', methods=['GET'])
@@ -14,4 +17,3 @@ def ping_pong():
         'status': 'success',
         'message': 'pong!'
 })
-
