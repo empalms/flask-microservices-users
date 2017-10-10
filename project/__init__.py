fe46399335333ae4b53478d1a6e591b92307043d
empalms <ev@npalmer.io>
2017-10-09 22:38:42 -0500

    Configured postgres/sqlalchemy and added users table

--- project/__init__.py
+++ project/__init__.py
@@ -1,15 +1,34 @@
 import os
+import datetime
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 # instantiate the application
 app = Flask(__name__)
 
+# Set config
+app_settings = os.getenv('APP_SETTINGS')  # pull in environment variables
+app.config.from_object('project.config.DevelopmentConfig') # set configuration from config.py
 
-# pull in environment variables
-app_settings = os.getenv('APP_SETTINGS')
-# set configuration from config.py
-app.config.from_object('project.config.DevelopmentConfig')
 
+# instantiate the database
+db = SQLAlchemy(app)
+
+# user model
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=False, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+        self.created_at = datetime.datetime.utcnow()
+
+# routes
 
 @app.route('/ping', methods=['GET'])
 def ping_pong():
--- project/config.py
+++ project/config.py
@@ -1,21 +1,26 @@
+import os
 
 class BaseConfig:
     """Base Configuration"""
     DEBUG = False
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(BaseConfig):
     """Development Configuation"""
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 class TestingConfig(BaseConfig):
     """Testing Configuration"""
     DEBUG = True
     Testing = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 
 class ProductionConfig(BaseConfig):
     """Production Configuration"""
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
